9508136ba409d48aec44f2bd4665610c1b38907f
Michael Lee <michael.lee@michael.lee-FMF6J19R7N>
2025-09-06 20:14:01 -0400

    add harpoon to telescope

--- init.lua
+++ init.lua
@@ -441,6 +441,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'harpoon')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
